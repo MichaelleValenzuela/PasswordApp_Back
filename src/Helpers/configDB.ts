007248ef8f3fc7fdc3288ba39ba6b7a28ab15ddd
Michaelle Valenzuela <michaelle.valenzuela@epn.edu.ec>
2024-02-06 21:26:06 -0500

    Despliegue back

---
+++ src/Helpers/configDB.ts
@@ -0,0 +1,107 @@
+import mongoose from "mongoose";
+import Admin_inputResource from "../CORE/Model/AdminResource.model";
+import { varsConfig } from "./varsConfig";
+import dotenv from 'dotenv'
+import 'dotenv/config'
+
+
+
+console.log(process.env.APP_SERVER_MONGODB_URI)
+
+export const configDB = async () => {
+    try {
+//        dotenv.config()
+        //await mongoose.connect(varsConfig.APP_SERVER_MONGODB_URI);
+        await mongoose.connect(varsConfig.ADRESS_MONGO as string);
+        //const db = await mongoose.connect(process.env.ADRESS_MONGO);
+
+        console.log("DB is connected ;)");
+
+        Admin_inputResource.findOne({ name_resource: "Tipo de contraseña" || "Tipo de tarjeta" || "Nota" }).exec().then((dat: any) => {
+            if (!dat) {
+                Admin_inputResource.insertMany([
+                    {
+                        name_resource: "Tipo de contraseña",
+                        name_type_encrypt: "JWT",
+                        fields: [
+                            {
+                                label_input: "Nombre del recurso",
+                                name_input: "nombre_del_recurso",
+                                type_input: "text"
+                            },
+                            {
+                                label_input: "Usuario",
+                                name_input: "usuario",
+                                type_input: "text"
+                            },
+                            {
+                                label_input: "Contraseña",
+                                name_input: "contrasenia",
+                                type_input: "password"
+                            }
+                        ]
+                    },
+                    {
+                        name_resource: "Tipo de tarjeta",
+                        name_type_encrypt: "JWT",
+                        fields: [
+                            {
+                                label_input: "Nombre del recurso",
+                                name_input: "nombre_del_recurso",
+                                type_input: "text",
+                            },
+                            {
+                                label_input: "Nombre de la tarjeta",
+                                name_input: "nombre_tarjeta",
+                                type_input: "text",
+                            },
+                            {
+                                label_input: "Nro de la tarjeta",
+                                name_input: "nrodetarjeta",
+                                type_input: "password"
+                            },
+                            {
+                                label_input: "Mes de expiración",
+                                name_input: "mesdeexpiracion",
+                                type_input: "text"
+                            },
+                            {
+                                label_input: "Año de expiración",
+                                name_input: "aniodeexpiracion",
+                                type_input: "text",
+                            },
+                            {
+                                label_input: "Código de seguridad",
+                                name_input: "codigodeseguridad",
+                                type_input: "password"
+                            }
+                        ]
+                    },
+                    {
+                        name_resource: "Nota",
+                        name_type_encrypt: "JWT",
+                        fields: [
+                            {
+                                label_input: "Nombre del recurso",
+                                name_input: "nombre_del_recurso",
+                                type_input: "text"
+                            },
+                            {
+                                label_input: "Descripción",
+                                name_input: "descripcion",
+                                type_input: "text"
+                            }
+                        ]
+                    }
+                ]).then(function () {
+                    console.log("Data inserted")  // Success 
+                }).catch(function (error) {
+                    console.log(error) // Failure 
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import cors from "cors";
+import express, { Router } from "express";
+import bodyParser from "body-parser";
+import { useRoutes } from "./CORE/useRoutes";
+
+import { varsConfig } from "./Helpers/varsConfig";
+import { configDB } from "./Helpers/configDB";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT || varsConfig.PORT_EXPRESS;
+
+const app = express();
+const router = Router();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+useRoutes(app, router);
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Authorization,usuario_autorizacion,  X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
+
+//configDB();
+
+app.listen(port, async () => {
+    console.log(`Running server on [${varsConfig.PORT_EXPRESS}]`);
+    configDB();
+});
